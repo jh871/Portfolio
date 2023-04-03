3ac0c6b80b41dcf09aa910276f20b67708220460
jh871 <jennyh871@gmail.com>
2023-04-03 18:37:54 +0100

    navbar code in and commented

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,145 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+
+import Button from '@mui/material/Button';
+
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+
 
 import './Navbar.css'
 
+// Pages shown in bar and in burger menu
+const pages = ['Products', 'Pricing', 'Blog'];
+
+// Makes appbar responsive by handling burger menu
 function Navbar() {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+
     return ( 
-        <>
-        
-        </>
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+
+                    {/*fullscreen: Icon and word 'Logo'*/}
+                    <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                    <Typography
+                    variant="h6"
+                    noWrap
+                    component="a"
+                    href="/"
+                    sx={{
+                    mr: 2,
+                    display: { xs: 'none', md: 'flex' }, //dont display when screen tiny
+                    fontFamily: 'monospace',
+                    fontWeight: 700,
+                    letterSpacing: '.3rem',
+                    color: 'inherit',
+                    textDecoration: 'none',
+                    }}
+                    >
+                    LOGO
+                    </Typography>
+
+
+{/*this box is the hamburger menu: */}
+        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton     //hamburger icon
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+            >
+                <MenuIcon />
+            </IconButton>
+            <Menu       //menu items in hamburger menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                display: { xs: 'block', md: 'none' },
+                }}
+            >
+                {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+                ))}
+            </Menu>
+            </Box> {/*End of hamber menu*/}
+
+
+            {/*stops icon displaying when big screen: */}
+            <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} /> 
+
+            {/* Logo just for mobile: */}
+            <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+                mr: 2,
+                display: { xs: 'flex', md: 'none' },
+                flexGrow: 1,
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+            }}
+            >
+            LOGO
+            </Typography>
+
+            {/*Fullscreen menu items: */}
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+                <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                {page}
+                </Button>
+            ))}
+            </Box>
+        </Toolbar>
+        </Container>
+    </AppBar>
     );
 }
 
 export default Navbar;
+
+
+
+
