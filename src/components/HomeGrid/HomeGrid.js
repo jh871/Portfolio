8ccbd7b7c5823830d5e6e58744fd0d5074cd8442
jh871 <jennyh871@gmail.com>
2023-04-04 18:38:45 +0100

    updated styling on home page incl right panel

--- src/components/HomeGrid/HomeGrid.js
+++ src/components/HomeGrid/HomeGrid.js
@@ -1,30 +1,25 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
-// import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
+
 import BasicStack from './LeftPanel/LeftPanel';
+import RightPanel from './RightPanel/RightPanel';
 import Grid from '@mui/material/Grid';
 import './HomeGrid.css'
 
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-}));
+
 
 
 function HomeGrid() {
     return (
     <Grid container spacing={0}>
         <Grid className='grid-left' item md={5} >
-            <Item className='left-panel'>
+            <div className='left-panel'>
                 <BasicStack />
-            </Item>
+            </div>
         </Grid>
         <Grid className='grid-right' item xs={12} md={7} >
-            <Item className='right-panel'>Right panel</Item>
+            <div className='right-panel'>
+                <RightPanel />
+            </div>
         </Grid>
     </Grid>
     );
---
+++ src/components/HomeGrid/RightPanel/RightPanel.js
@@ -0,0 +1,32 @@
+
+import './RightPanel.css'
+
+function RightPanel() {
+    return ( 
+    <div className="intro-div">
+        <h1 className="intro-header">Hi, I'm Jen. <br /><br /> I do front-end web development.</h1>
+        <div className='text-div'>
+            <p className="home-text">
+                I enjoy designing and building web page applications with JavaScript and React. After completing a skills certificate in Front-End web dev I am currently working on additional projects. 
+                <br />
+                <br />
+                My aim is to try and expand my skills into back-end so I can create more advanced apps. I am excited to keep learning!
+                <br />
+                <br />
+                I can usually be found somewhere in Suffolk holding a hot chocolate and waving at other people's dogs. I am available for local or remote work.
+                <br />
+                <br />
+                </p>
+                <h3 className='more-label'>Learn more:</h3>
+                <div className='learn-more'>
+                    <h3 className='cv more-item'> CV </h3>
+                    <h3 className='skills more-item'>Skills</h3>
+                    <h3 className='projects more-item'>Projects</h3>
+                    <h3 className='contact more-item'>Contact</h3>
+                </div>
+        </div>
+    </div>
+    );
+}
+
+export default RightPanel;
