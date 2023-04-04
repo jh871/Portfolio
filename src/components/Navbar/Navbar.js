087990cbe4974f6e969b32cb71191c555f39fd68
jh871 <jennyh871@gmail.com>
2023-04-04 12:04:49 +0100

    matched xs menu styling and tested links

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -111,21 +111,19 @@ function Navbar() {
 
 
             {/*stops icon displaying when big screen: */}
-            <BubbleChartIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} /> 
+            <BubbleChartIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1, fontSize: '2rem' }} /> 
 
             {/* Logo just for mobile: */}
             <Typography
             variant="h5"
             noWrap
             component="a"
-            href=""
+            href="/"
             sx={{
                 mr: 2,
                 display: { xs: 'flex', md: 'none' },
                 flexGrow: 1,
-                fontFamily: 'monospace',
-                fontWeight: 700,
-                letterSpacing: '.3rem',
+                fontFamily: 'Tourney',
                 color: 'inherit',
                 textDecoration: 'none',
             }}
