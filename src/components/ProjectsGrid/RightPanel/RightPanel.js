862158db2305eaefaf11d52e937e40951a5c8ff5
jh871 <jennyh871@gmail.com>
2023-04-09 22:30:36 +0100

    modal element created but couldn't lauch correctly yet

--- src/components/ProjectsGrid/RightPanel/RightPanel.js
+++ src/components/ProjectsGrid/RightPanel/RightPanel.js
@@ -4,24 +4,24 @@ import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
 import IconButton from '@mui/material/IconButton';
 import PreviewIcon from '@mui/icons-material/Preview';
-
 import './RightPanel.css'
-// import ImgArray from '../../../image_data.json'
+
+
 
 function srcset(image, size, rows = 1, cols = 1) {
     return {
     src: `process.env.PUBLIC_URL${image}?w=${size * cols}&h=${size * rows}&fit=crop&auto=format`,
     srcSet: `${image}?w=${size * cols}&h=${
-      size * rows
+        size * rows
     }&fit=crop&auto=format&dpr=2 2x`,
     };
 }
 
 export default function RightPanel() {
-    // console.log(ImgArray[0].title);
+
     return (
-    
     <div className='img-list-div'>
+        {/* Image list container */}
         <ImageList
             className='img-list'
             sx={{ width: 490, height: 580 }}
@@ -29,26 +29,27 @@ export default function RightPanel() {
             cols={4}
             rowHeight={140}
         >
+            {/* creating each tile */}
             {itemData.map((item) => (
-            <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
+                <ImageListItem key={item.img} cols={item.cols || 1} rows={item.rows || 1}>
                 <img
                 {...srcset(item.img, 121, item.rows, item.cols)}
                 alt={item.title}
                 loading="lazy"
-                // style={{objectFit:"cover"}}
                 />
-            <ImageListItemBar
+                
+            {/* Label infobar */}
+                <ImageListItemBar
                 title={item.title}
                 subtitle={item.skills}
                 actionIcon={
                 <IconButton
                     sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                    aria-label={`info about ${item.title}`}
                 >
                     <PreviewIcon />
                 </IconButton>
                 }
-            />
+                />
             </ImageListItem> 
             ))}
         </ImageList>
@@ -56,9 +57,11 @@ export default function RightPanel() {
     )
 }
 
+// object of project info
 const itemData = [
     {
         "img": "/images/spark_snippetthumbnail.png",
+        // "id": 1,
         "title": "Spark",
         "skills": "React",
         "rows": 1,
@@ -66,14 +69,15 @@ const itemData = [
     },
     {
         "img": "/images/codequiz_snippet.png",
+        // "id": 2,
         "title": "Code Quiz",
         "skills": "JavaScript",
         "rows": 1,
         "cols": 2
-
     },
     {
         "img": "/images/team-profile-gen_snippet.png",
+        // "id": 3,
         "title": "Team Profile Generator",
         "skills": "Node.js",
         "rows": 1,
@@ -81,6 +85,7 @@ const itemData = [
     },
     {
         "img": "/images/PokeQuiz_snippet.png",
+        // "id": 4,
         "title": "PokéQuiz",
         "skills": "JavaScript",
         "rows": 1,
@@ -88,6 +93,7 @@ const itemData = [
     },
     {
         "img": "/images/JHull-bootstrap_portfolio_snippet.png",
+        // "id": 5,
         "title": "Bootstrap Portfolio",
         "skills": "Bootstrap CSS",
         "rows": 1,
@@ -95,6 +101,7 @@ const itemData = [
     },
     {
         "img": "/images/dayplanner_snippet.png",
+        // "id": 6,
         "title": "Work Day Planner",
         "skills": "JQuery",
         "rows": 1,
