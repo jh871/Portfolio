af40bea1f480dbefab704e27729c6868f59c9d39
jh871 <jennyh871@gmail.com>
2023-04-10 14:00:05 +0100

    content added for indv project page

--- src/components/IndvProject/RightPanel/RightPanel.js
+++ src/components/IndvProject/RightPanel/RightPanel.js
@@ -1,6 +1,9 @@
 import * as React from 'react';
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import projects from '../../../image_data.json';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 // import './RightPanel.css';
 
 function RightPanel() {
@@ -10,14 +13,38 @@ function RightPanel() {
     
     return ( 
         <div className='project-div'>
-            <button>back</button>
-
-            <div className='text-div'>
-                <h2>{project.title}</h2>
-            </div>
-        
+            <Link to={`/projects`}>
+                <button className='back-button'>
+                    <h3 className='button-text'><ArrowBackIcon />Back</h3>
+                </button>
+            </Link>
+            <Box sx={{ width: '100%' }}>
+                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                    <Grid item xs={12}>
+                    <div className='header-div'>
+                        <img 
+                            className='header-img'
+                            src={process.env.PUBLIC_URL + `${project.img}`}
+                            alt="screenshot of project" />
+                            <h1 className='header-title'>{project.title}</h1>
+                    </div>
+                    </Grid>
+                    <Grid item xs={12}>
+                    <div className='description-div'>
+                        <h3 className='description-text'>{project.description}</h3>
+                    </div>
+                    </Grid>
+                    <Grid item xs={6}>
+                    <div className='deploy-link'>Link to deployed app: {project.deployed}</div>
+                    </Grid>
+                    <Grid item xs={6}>
+                    <div className='repo-link'>Link to Repo: {project.repo}</div>
+                    </Grid>
+                </Grid>
+            </Box>     
         </div>
     );
 }
-// ; 
+
 export default RightPanel;
+//
