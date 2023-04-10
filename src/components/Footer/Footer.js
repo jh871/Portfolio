838d3f3ecb0796be70c8b081c853d0d5daf5ac84
jh871 <jennyh871@gmail.com>
2023-04-10 19:52:23 +0100

    footer content and styling updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,9 +4,13 @@ import './Footer.css'
 function Footer() {
     return ( 
         <div className='footer-div'>
-            <h2 className='footer-text'>Footer!</h2>
+            <div className='footer-text'>  
+                <h3 className='footer-h3 footer-credit'> Jennifer Hull 2023 || 
+                    <a href='https://github.com/jh871/Portfolio/' target='_blank' rel="noreferrer" className='footer-link'> visit the repo</a>
+                </h3>
+            </div>
+            <h6 className='footnote'>If you're reading this: this page is finished but could use some further development, check back later! </h6>
         </div>
     );
 }
-
 export default Footer;
